8140ade745c6d0e2efc5c9627877c3b700e47a30
Tola Awofolu <oawofolu@pivotal.io>
2022-06-09 07:32:36 -0400

    Updated transform

---
+++ script.sh
@@ -0,0 +1,2 @@
+cat jupyter-deployment.yaml | yq -y '.spec.template.spec.containers[0].volumeMounts += [{"mountPath":"/etc/jupyterhub/bindings","name":"YOUR_DATASOURCE"}] | .spec.template.spec.volumes += [{"name":"pginstance-1","secret":{"secretName":"YOUR_DATASOURCE-app-user-db-secret"}}]' > tmp.yaml
+mv tmp.yaml jupyter-deployment.yaml
