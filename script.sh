d4d93a8f0051fc53c5095b7ba790ae4e47eda611
Tola Awofolu <oawofolu@pivotal.io>
2022-06-09 17:24:34 -0400

    Fixes to SecretImport/SecretExport

--- script.sh
+++ script.sh
@@ -1,1 +1,3 @@
-kubectl apply -f datasource-secrets.yaml -f jupyter-deployment.yaml -f jupyter-deployment-service-binding.yaml -nYOUR_SESSION_NAMESPACE
+kubectl apply -f datasource-secrets.yaml
+kubectl apply -f jupyter-deployment.yaml -nYOUR_SESSION_NAMESPACE
+kubectl apply -f jupyter-deployment-service-binding.yaml -nYOUR_SESSION_NAMESPACE
