e9b4394c63b8f38b7e456e7aa79f9a2b568ff44f
Tola Awofolu <oawofolu@pivotal.io>
2022-06-09 08:06:22 -0400

    Script update

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-kubectl apply -f datasource.yaml
+kubectl get secret YOUR_DATASOURCE-app-user-db-secret -ndata-samples-w04-s005 -oyaml | sed 's/namespace: .*/namespace: YOUR_SESSION_NAMESPACE/' | kubectl apply -nYOUR_SESSION_NAMESPACE -f -
 cat jupyter-deployment.yaml | yq -y '.spec.template.spec.containers[0].volumeMounts += [{"mountPath":"/etc/jupyterhub/bindings","name":"YOUR_DATASOURCE"}] | .spec.template.spec.volumes += [{"name":"pginstance-1","secret":{"secretName":"YOUR_DATASOURCE-app-user-db-secret"}}]' > tmp.yaml
 mv tmp.yaml jupyter-deployment.yaml
 kubectl apply -f jupyter-deployment.yaml
