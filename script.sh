d19a84cfc3013d84fb8dbdc4b1547495897c2bc3
Tola Awofolu <oawofolu@pivotal.io>
2022-06-09 07:42:22 -0400

    Updated script to include Jupyter deployment patches

--- script.sh
+++ script.sh
@@ -1,2 +1,4 @@
+kubectl apply -f datasource.yaml
 cat jupyter-deployment.yaml | yq -y '.spec.template.spec.containers[0].volumeMounts += [{"mountPath":"/etc/jupyterhub/bindings","name":"YOUR_DATASOURCE"}] | .spec.template.spec.volumes += [{"name":"pginstance-1","secret":{"secretName":"YOUR_DATASOURCE-app-user-db-secret"}}]' > tmp.yaml
 mv tmp.yaml jupyter-deployment.yaml
+kubectl apply -f jupyter-deployment.yaml
